ef546c3862b50cc1fbd2af5f3ec8560263ad3c47
Andrew Gustafson <gus4000@gmail.com>
2026-06-30 18:12:06 +0200

    test: add enum constant body examples

---
+++ examples/inputs/java8/Issue47EnumClassBodyConstants.java
@@ -0,0 +1,3 @@
+package p;
+
+enum E { A { void run(){ return; } }, B { void run(){ return; } }; void base(){ return; } }
---
+++ examples/outputs/java17/Issue47EnumClassBodyConstants.java
@@ -0,0 +1,17 @@
+package p;
+
+enum E {
+    A {
+        void run() {
+            return;
+        }
+    },
+    B {
+        void run() {
+            return;
+        }
+    };
+    void base() {
+        return;
+    }
+}
